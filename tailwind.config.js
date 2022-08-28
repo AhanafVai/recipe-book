ec6301e539e126c9d8810d986b61f78103926061
ahanaf <ahanafabdullah9@gmail.com>
2022-08-28 21:37:47 +0600

    Finished mobile design

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./*{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {},
+
+      colors: {},
+    },
+  },
+  plugins: [],
+};
