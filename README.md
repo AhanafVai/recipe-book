f36348bdbe0e61dbd490f4a73fe5d2a0e1763e3d
ahanaf <ahanafabdullah9@gmail.com>
2022-08-30 11:41:20 +0600

    Implemented JS and Desktop Design

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## TODO
 
+- Design the tablet layout
 - Design the desktop layout
 - Integrate the MealDB API
--- app.js
+++ app.js
@@ -1,51 +1,98 @@
 // Getting Element
 const cardContainer = document.querySelector("#cardContainer");
 const modalContainer = document.querySelector("#modalContainer");
-
-// Card
-const cardDiv = document.createElement("div");
-cardDiv.innerHTML = `<figure class="max-w-sm cursor-pointer mx-auto" onclick="modalDiv.show()">                
-                <img class="rounded-3xl" src="/images/trend-5.jpg" alt="trend-5">
-                  <figcaption class="w-full text-lg text-center font-semibold text-gray-800 ">
-                   <p class="my-2">Bengali bowl</p>
-                  </figcatpion>
-              </figure> `;
-cardContainer.appendChild(cardDiv);
-
-// Modal
 const modalDiv = document.createElement("dialog");
-modalDiv.classList.add("modal");
-modalDiv.show();
-modalDiv.innerHTML = `
-<div class="modal-content rounded-xl p-6 mx-auto">
+const error = document.querySelector("#error");
+
+// Search function
+const search = () => {
+  const searchMenu = document.getElementById("searchMenu").value;
+  searchMenu.value = "";
+  if (searchMenu === "") {
+    error.innerText = "Enter Category plz";
+  } else {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMenu}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayMenu(data.meals));
+  }
+};
+
+// Menu
+const displayMenu = (recipes) => {
+  if (recipes === null) {
+    cardContainer.innerHTML = `<p class="text-4xl text-center my-14 text-slate-300 font-bold"><q>sorry not found!</q> </p>`;
+  } else {
+    recipes.forEach((recipe) => {
+      const cardDiv = document.createElement("div");
+      cardDiv.innerHTML = `<figure class="max-w-sm cursor-pointer mx-auto"  onclick="mealDetails(${recipe.idMeal})">                
+                <img class="rounded-3xl" src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
+                  <figcaption class="w-full text-lg text-center font-semibold text-gray-800 ">
+                   <p class="my-2">${recipe.strMeal}</p>
+                  </figcatpion>
+              </figure>  `;
+      cardContainer.appendChild(cardDiv);
+    });
+  }
+};
+
+const mealDetails = (id) => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then((res) => res.json())
+    .then((data) => recipe(data.meals));
+};
+
+//  Modal
+const recipe = (items) => {
+  if (items) {
+    modalDiv.show();
+    items.forEach((item) => {
+      modalDiv.classList.add("modal");
+      modalDiv.innerHTML = `
+<div class="modal-content rounded-xl p-6 mx-auto md:grid md:grid-cols-2 md:space-x-14 ">
   <figure class="max-w-md cursor-pointer mx-auto"   ">                
-    <figcaption class="w-full text-lg text-left font-semibold text-gray-800 ">
-      <p class="my-2">Bengali bowl</p>
+    <figcaption class="w-full text-lg text-left font-semibold text-gray-800  md:text-2xl ">
+      <p class="my-2 md:mb-10">${item.strMeal}</p>
     </figcatpion>
-     <img class="rounded-3xl" src="/images/trend-5.jpg" alt="trend-5">
+     <img class="rounded-3xl md:rounded-none" src="${item.strMealThumb}" alt="${
+        item.strMeal
+      }">
   </figure>
-  <div>
-   <div class="  my-5 space-x-2">
-    <button type="button" class="p-1  text-sm  text-slate-300 focus:outline-none bg-gray-800 rounded-lg focus:bg-slate-300 focus:text-gray-800" onclick="getElementById('instructions').classList.toggle('hidden'),getElementById('ingredients').classList.add('hidden')">Instructions</button>
-    <button type="button" class="p-1  text-sm  text-slate-300 focus:outline-none bg-gray-800 rounded-lg" onclick="getElementById('ingredients').classList.toggle('hidden'),getElementById('instructions').classList.add('hidden')">Ingredients</button>
-    <button type="button" class="p-1  text-sm  text-slate-300 focus:outline-none bg-gray-800 rounded-lg">
-      <a target="_blank" href="https://www.youtube.com/watch?v=27tXqfc3vpY&list=RDMM27tXqfc3vpY&start_radio=1">Video</a>
+ <div>
+   <div class=" my-5 space-x-2">
+    <button type="button" class="p-1  text-sm  text-slate-300 focus:outline-none bg-gray-800 rounded-lg focus:bg-slate-300 focus:text-gray-800 md:rounded-none lg:text-lg lg:p-2" onclick="getElementById('instructions').classList.toggle('hidden'),getElementById('ingredients').classList.add('hidden')">Instructions</button>
+    <button type="button" class="p-1  text-sm  text-slate-300 focus:outline-none bg-gray-800 rounded-lg md:rounded-none lg:text-lg lg:p-2" onclick="getElementById('ingredients').classList.toggle('hidden'),getElementById('instructions').classList.add('hidden')">Ingredients</button>
+    <button type="button" class="p-1  text-sm  text-slate-300 focus:outline-none bg-gray-800 rounded-lg md:rounded-none lg:text-lg lg:p-2">
+      <a target="_blank" href="${item.strYoutube}">Video</a>
     </button>
    </div>
    <article id="instructions" class="hidden px-1 text-justify">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus optio ab cum libero architecto, facilis eos qui officiis quas magni!</p>
+   <details>
+     <summary class="text-sm lg:text-lg">${item.strInstructions.slice(
+       350
+     )}</summary>
+  <p class="text-sm lg:text-lg">${item.strInstructions.slice(-350)}</p>
+</details>
+ 
    </article>
    <ul id="ingredients" class="hidden">
-    <li>lorem</li>
-    <li>paddind</li>
-    <li>blingds</li>
-    <li>fuckoff</li>
+  <li>${item.strIngredient1}</li>
+        <li>${item.strIngredient2}</li>
+        <li>${item.strIngredient3}</li>
+        <li>${item.strIngredient4}</li>
+        <li>${item.strIngredient5}</li>
+        <li>${item.strIngredient6}</li>
+        <li>${item.strIngredient7}</li>
+        <li>${item.strIngredient8}</li>
+        <li>${item.strIngredient9}</li>
+        <li>${item.strIngredient10}</li>
    </ul>
 
-   <button class="p-1.5 my-5 text-sm text-white focus:outline-none bg-red-700 rounded-lg" onclick="modalDiv.close()">close modal</button>
+   <button class="p-1.5 my-5 text-sm text-white focus:outline-none bg-red-700 rounded-lg  md:rounded-none lg:text-lg md:p-2" onclick="modalDiv.close()">close modal</button>
   </div>
- 
-  
 </div>
 `;
-modalContainer.appendChild(modalDiv);
+      modalContainer.appendChild(modalDiv);
+    });
+  }
+};
